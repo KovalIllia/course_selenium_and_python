8ade6269ada8e9e1bce219b6b503cbb3a9941ce6
koval_i <koval.illia.mailbox@gmail.com>
2025-05-04 22:18:58 +0300

    Add new files (contains homework assignments before the beginning of chapter 3)

---
+++ lesson3.2__step3.py
@@ -0,0 +1,83 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+class UnitestTest(unittest.TestCase):
+    def test_registration1(self):
+        try:
+            link = "http://suninjuly.github.io/registration1.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            # Ваш код, который заполняет обязательные поля
+            input_first_name = browser.find_element(By.XPATH, "//input[@placeholder='Input your first name']")
+            input_first_name.send_keys("Kla")
+            input_last_name = browser.find_element(By.XPATH, "//input[@placeholder='Input your last name']")
+            input_last_name.send_keys("Kla")
+            input_email = browser.find_element(By.XPATH, "//input[@placeholder='Input your email']")
+            input_email.send_keys("lalala@gmail.com")
+            # Отправляем заполненную форму
+            button = browser.find_element(By.XPATH, "//button[@type='submit']")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            assert "Congratulations! You have successfully registered!" == welcome_text
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(10)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+    def test_registration1(self):
+        try:
+            link = "http://suninjuly.github.io/registration2.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            # Ваш код, который заполняет обязательные поля
+            input_first_name = browser.find_element(By.XPATH, "//input[@placeholder='Input your first name']")
+            input_first_name.send_keys("Kla")
+            input_last_name = browser.find_element(By.XPATH, "//input[@placeholder='Input your last name']")
+            input_last_name.send_keys("Kla")
+            input_email = browser.find_element(By.XPATH, "//input[@placeholder='Input your email']")
+            input_email.send_keys("lalala@gmail.com")
+            # Отправляем заполненную форму
+            button = browser.find_element(By.XPATH, "//button[@type='submit']")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            assert "Congratulations! You have successfully registered!" == welcome_text
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(10)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+#final responce: FAILED (errors=1)
